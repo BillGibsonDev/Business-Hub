fa67eb173b9fe0ade9e917af65787c36e92a4be2
Bill Gibson <gibsonrwilliam@hotmail.com>
2021-10-25 08:25:06 -0400

    customizable

--- app.js
+++ app.js
@@ -4,37 +4,46 @@ const linkInput = document.querySelector(".link-input");
 const list = document.querySelector(".list-container");
 const addButton = document.querySelector(".add");
 const colorInput = document.querySelector(".color-input");
+const showAddButton = document.querySelector(".show-add-button");
+
 
 // event listeners
 addButton.addEventListener("click", addLink);
-
+showAddButton.addEventListener("click", showInput);
 
 // functions
 function addLink(e) {
-    e.preventDefault();
-    // create div
-    const listDiv = document.createElement("div");
-    listDiv.classList.add("link");
-    // create link
-    const newLink = document.createElement("a");
-    newLink.innerText = nicknameInput.value;
-    newLink.href = linkInput.value;
-    newLink.target = "_blank";
-    newLink.style.color = colorInput.value;
-    // attach final
-    newLink.classList.add("link-item");
-    listDiv.appendChild(newLink);
-    linkInput.value = "";
-    nicknameInput.value = "";
-    colorInput.value = "";
-    // create trash button
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-    trashButton.addEventListener("click" , deleteLink);
-    trashButton.classList.add("trash-btn");
-    listDiv.appendChild(trashButton);
-    //attach everything
-    list.appendChild(listDiv);
+  e.preventDefault();
+  // create div
+  const listDiv = document.createElement("div");
+  listDiv.classList.add("link");
+  // create link
+  const newLink = document.createElement("a");
+  newLink.innerText = nicknameInput.value;
+  newLink.href = linkInput.value;
+  newLink.target = "_blank";
+  newLink.style.color = colorInput.value;
+  // 
+  newLink.classList.add("link-item");
+  listDiv.appendChild(newLink);
+  // reset inputs
+  linkInput.value = "";
+  nicknameInput.value = "";
+  colorInput.value = "";
+  // create trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  trashButton.addEventListener("click" , deleteLink);
+  trashButton.classList.add("trash-btn");
+  trashButton.classList.add("link-button");
+  listDiv.appendChild(trashButton);
+  // color input
+  const colorButton = document.createElement("input");
+  colorButton.type = "color";
+  colorButton.classList.add("link-button");
+  listDiv.appendChild(colorButton);
+  //attach everything
+  list.appendChild(listDiv);
 }
 
 function deleteLink(e) {
@@ -42,3 +51,14 @@ function deleteLink(e) {
   const link = item.parentElement.parentElement; // lol
   link.remove();
 };
+
+
+function showInput() {
+  const inputContainer = document.querySelector(".input-container");
+  if (inputContainer.style.display === "flex") {
+    inputContainer.style.display = "none"
+  } else {
+    inputContainer.style.display = "flex";
+  }
+}
+
